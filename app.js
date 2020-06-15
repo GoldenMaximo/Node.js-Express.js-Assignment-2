4eb81e009d2edb8ef1a6b3a6a949bce2c79d6edc
Gustavo Máximo <gfmaximo97@gmail.com>
2020-06-15 16:29:28 -0300

    Challenge completed.

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+
+app.use('/users', (req, res) => {
+    res.send('<h1>Now this is the users page</h1>');
+});
+
+app.use('/', (req, res) => {
+    res.send('<h1>Howdy, this is the slash page, you can go to the <a href="/users">users page</a> if you want</h1>');
+});
+
+
+app.listen(666);
